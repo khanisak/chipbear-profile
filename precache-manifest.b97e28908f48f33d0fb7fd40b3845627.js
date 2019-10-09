9a24daceaded5d0e8dba9571c27c3fa45cb90ea8
khanisak <khanisjebol@yahoo.com>
2019-10-09 23:08:58 +0700

    Updates

---
+++ precache-manifest.b97e28908f48f33d0fb7fd40b3845627.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af27b56f2890fc1fdc191f4b651d4186",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12f4df777b7f9f14c814",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "d6c0078f4006c02a8666",
+    "url": "/static/js/2.01edf5b7.chunk.js"
+  },
+  {
+    "revision": "12f4df777b7f9f14c814",
+    "url": "/static/js/main.f896268a.chunk.js"
+  },
+  {
+    "revision": "4e09829eea494ec5d21e",
+    "url": "/static/js/runtime-main.9314c955.js"
+  }
+]);
